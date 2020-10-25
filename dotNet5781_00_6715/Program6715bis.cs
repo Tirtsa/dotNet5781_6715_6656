705eea8067cea1605d20b0d3e61bf94b61e2d03b
Tirtsa <37661158+Tirtsa@users.noreply.github.com>
2020-10-25 17:52:50 +0200

    Create Program6715bis.cs

---
+++ dotNet5781_00_6715/Program6715bis.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_6715
+{
+    partial class Program
+    {
+
+        static partial void Welcome6715bis()
+        {
+          Console.WriteLine("I am also here");
+        }
+    }
+}
