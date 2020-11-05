1f1fc83838afd86f9ca0fbea780615564fd7f0f5
Tirtsah Barzilai <tirtsabarzilai@gmail.com>
2020-11-05 19:00:57 +0200

    Create ex2

---
+++ dotNet5781_02_6715_5227/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_6715_5227
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
