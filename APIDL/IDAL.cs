9120f8302cbdb11bf50fb4d061d1cae4c64d209c
Tirtsah Barzilai <tirtsabarzilai@gmail.com>
2021-01-05 00:04:35 +0200

    End of DalObject

--- APIDL/IDAL.cs
+++ APIDL/IDAL.cs
@@ -27,32 +27,32 @@ namespace APIDL
         #endregion
 
         #region BusLine
-        IEnumerable<DO.BusStation> GetAllLines();
-        IEnumerable<DO.BusStation> GetAllLinesBy(Predicate<DO.BusStation> predicate);
-        DO.BusStation GetLine(int id);
+        IEnumerable<DO.BusLine> GetAllLines();
+        IEnumerable<DO.BusLine> GetAllLinesBy(Predicate<DO.BusLine> predicate);
+        DO.BusLine GetLine(int id);
         void AddLine(DO.BusLine line);
-        void UpdateLine(DO.BusStation station);
-        void UpdateLine(int id, Action<DO.BusStation> update);
+        void UpdateLine(DO.BusLine line);
+        void UpdateLine(int id, Action<DO.BusLine> update);
         void DeleteLine(int id);
         #endregion
 
         #region LineStation
-        IEnumerable<DO.BusStation> GetAllLineStations();
-        IEnumerable<DO.BusStation> GetAllLineStationsBy(Predicate<DO.BusStation> predicate);
-        DO.BusStation GetLineStation(int id);
+        IEnumerable<DO.LineStation> GetAllLineStations();
+        IEnumerable<DO.LineStation> GetAllLineStationsBy(Predicate<DO.LineStation> predicate);
+        DO.LineStation GetLineStation(int id);
         void AddLineStation(DO.LineStation lineStation);
-        void UpdateLineStation(DO.BusStation station);
-        void UpdateLineStation(int id, Action<DO.BusStation> update);
+        void UpdateLineStation(DO.LineStation station);
+        void UpdateLineStation(int id, Action<DO.LineStation> update);
         void DeleteLineStation(int id);
         #endregion
 
         #region FollowingStations
         IEnumerable<DO.FollowingStations> GetAllFollowingStations();
-        IEnumerable<DO.FollowingStations> GetAllFollowingStationsBy(Predicate<DO.BusStation> predicate);
+        IEnumerable<DO.FollowingStations> GetAllFollowingStationsBy(Predicate<DO.FollowingStations> predicate);
         DO.FollowingStations GetFollowingStations(int id);
         void AddFollowingStations(DO.BusStation station1, DO.BusStation station2);
-        void UpdateFollowingStations(DO.BusStation station);
-        void UpdateFollowingStations(int id, Action<DO.BusStation> update);
+        void UpdateFollowingStations(DO.BusStation station1, DO.BusStation station2);
+        void UpdateFollowingStations(int id, Action<DO.FollowingStations> update);
         void DeleteFollowingStations(int id);
         #endregion
     }
--- DalObject/DalObject.cs
+++ DalObject/DalObject.cs
@@ -20,8 +20,61 @@ namespace DL
         public static DalObject Instance => instance;
         #endregion
 
+        #region Station
+        public void AddStation(BusStation station)
+        {
+            if (DataSource.ListStations.FirstOrDefault(s => s.BusStationKey == station.BusStationKey) != null)
+                throw new ArgumentException("Duplicate Stations");
+            DataSource.ListStations.Add(station.Clone());
+        }
+
+        public void DeleteStation(int id)
+        {
+            var stationToDelete = DataSource.ListStations.Where(s => s.BusStationKey == id).FirstOrDefault();
+            DataSource.ListStations.Remove(stationToDelete);
+        }
+
+        public IEnumerable<BusStation> GetAllStations()
+        {
+            return from station in DataSource.ListStations
+                   select station.Clone();
+        }
+
+        public IEnumerable<BusStation> GetAllStationsBy(Predicate<BusStation> predicate)
+        {
+            return from station in DataSource.ListStations
+                   where predicate(station)
+                   select station.Clone();
+        }
+
+        public BusStation GetStation(int id)
+        {
+            return DataSource.ListStations.Find(s => s.BusStationKey == id).Clone();
+        }
+        public void UpdateStation(BusStation station)
+        {
+            BusStation sta = DataSource.ListStations.Find(s => s.BusStationKey == station.BusStationKey);
+            if (sta != null)
+            {
+                DataSource.ListStations.Remove(sta);
+                DataSource.ListStations.Add(station.Clone());
+            }
+            //else
+                //throw new exception that station to update doesn't exist 
+        }
+
+        public void UpdateStation(int id, Action<BusStation> update)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region FollowingStations
         public void AddFollowingStations(BusStation station1, BusStation station2)
         {
+            if (DataSource.ListFollowingStations.FirstOrDefault(f => f.KeyStation1 == station1.BusStationKey 
+                && f.KeyStation2 == station2.BusStationKey) != null)
+                //throw new exception to alert on duplicate stations
             DataSource.ListFollowingStations.Add( 
                 new FollowingStations
                 {
@@ -34,74 +87,15 @@ namespace DL
                     (new GeoCoordinate(station2.Latitude, station2.Longitude))*0.0012*0.5
                 });
         }
-
-        public void AddLine(BusLine line)
-        {
-            DataSource.ListLines.Add(
-                new BusLine
-                {
-                    Id = DS.DataSource.LineId++,
-                    BusLineNumber = line.BusLineNumber,
-                    Area = line.Area,
-                    FirstStationKey = line.FirstStationKey,
-                    LastStationKey = line.LastStationKey,
-                    TotalTime = line.TotalTime
-                }
-             );
-        }
-
-        public void AddLineStation(LineStation lineStation)
-        {
-            DataSource.ListLineStations.Add( 
-                new LineStation
-                {
-                    Id = DS.DataSource.LineStationId++,
-                    LineId = lineStation.LineId,
-                    StationKey = lineStation.StationKey,
-                    RankInLine = lineStation.RankInLine
-                });
-        }
-
-        public void AddStation(BusStation station)
-        {
-            DataSource.ListStations.Add( 
-                new BusStation
-                {
-                    BusStationKey = station.BusStationKey,
-                    Address = station.Address,
-                    StationName = station.StationName,
-                    Latitude = station.Latitude,
-                    Longitude = station.Longitude
-                });
-        }
-
         public void DeleteFollowingStations(int id)
         {
-            var followingStationToDelete = DataSource.ListFollowingStations.Where(t => t.Id == id).FirstOrDefault();
+            var followingStationToDelete = DataSource.ListFollowingStations.Where(f => f.Id == id).FirstOrDefault();
             DataSource.ListFollowingStations.Remove(followingStationToDelete);
         }
-
-        public void DeleteLine(int id)
-        {
-            var lineToDelete = DataSource.ListLines.Where(t => t.Id == id).FirstOrDefault();
-            DataSource.ListLines.Remove(lineToDelete);
-        }
-
-        public void DeleteLineStation(int id)
-        {
-            var lineStationToDelete = DataSource.ListLineStations.Where(t => t.Id == id).FirstOrDefault();
-            DataSource.ListLineStations.Remove(lineStationToDelete);
-        }
-
-        public void DeleteStation(int id)
-        {
-            var stationToDelete = DataSource.ListStations.Where(t => t.BusStationKey == id).FirstOrDefault();
-            DataSource.ListStations.Remove(stationToDelete);
-        }
-
         public IEnumerable<FollowingStations> GetAllFollowingStations()
         {
-            return DataSource.ListFollowingStations;
+            return from fs in DataSource.ListFollowingStations
+                   select fs.Clone();
         }
 
         public IEnumerable<FollowingStations> GetAllFollowingStationsBy(Predicate<FollowingStations> predicate)
@@ -110,98 +104,131 @@ namespace DL
                    where predicate(fs)
                    select fs.Clone();
         }
-
-        public IEnumerable<BusStation> GetAllLines()
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<BusStation> GetAllLinesBy(Predicate<BusStation> predicate)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<BusStation> GetAllLineStations()
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<BusStation> GetAllLineStationsBy(Predicate<BusStation> predicate)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<BusStation> GetAllStations()
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<BusStation> GetAllStationsBy(Predicate<BusStation> predicate)
-        {
-            throw new NotImplementedException();
-        }
-
         public FollowingStations GetFollowingStations(int id)
         {
-            throw new NotImplementedException();
+            return DataSource.ListFollowingStations.Find(f => f.Id == id).Clone();
+        }
+        public void UpdateFollowingStations(BusStation station1, BusStation station2)
+        {
+            FollowingStations folStat = DataSource.ListFollowingStations.Find(s => s.KeyStation1 == station1.BusStationKey
+            && s.KeyStation2 == station2.BusStationKey);
+            if (folStat != null)
+            {
+                DataSource.ListFollowingStations.Remove(folStat);
+                AddFollowingStations(station1, station2);
+            }
+            //else throx exception
         }
 
-        public BusStation GetLine(int id)
+        public void UpdateFollowingStations(int id, Action<FollowingStations> update)
         {
             throw new NotImplementedException();
         }
+        #endregion
 
-        public BusStation GetLineStation(int id)
+        #region Line
+        public void AddLine(BusLine line)
+        {
+            if (DataSource.ListLines.FirstOrDefault(l => l.Id == line.Id) != null)
+                //throw new DO.BadPersonIdException(person.ID, "Duplicate person ID");
+            DataSource.ListLines.Add(line.Clone());
+        }
+        public void DeleteLine(int id)
+        {
+            var lineToDelete = DataSource.ListLines.Where(t => t.Id == id).FirstOrDefault();
+            DataSource.ListLines.Remove(lineToDelete);
+        }
+        public IEnumerable<BusLine> GetAllLines()
+        {
+            return from line in DataSource.ListLines
+                   select line.Clone();
+        }
+
+        public IEnumerable<BusLine> GetAllLinesBy(Predicate<BusLine> predicate)
+        {
+            return from line in DataSource.ListLines
+                   where predicate(line)
+                   select line.Clone();
+        }
+        public BusLine GetLine(int id)
+        {
+            return DataSource.ListLines.Find(l => l.Id == id).Clone();
+        }
+        public void UpdateLine(BusLine line)
+        {
+            BusLine tempLine = DataSource.ListLines.Find(l => l.Id == line.Id);
+            if (tempLine != null)
+            {
+                DataSource.ListLines.Remove(tempLine);
+                DataSource.ListLines.Add(line.Clone());
+            }
+            //else throw exception that line doesn't exist
+        }
+
+        public void UpdateLine(int id, Action<BusLine> update)
         {
             throw new NotImplementedException();
         }
+        #endregion
 
-        public BusStation GetStation(int id)
+        #region LineStation
+        public void AddLineStation(LineStation lineStation)
+        {
+            if (DataSource.ListLineStations.FirstOrDefault(s => s.Id == lineStation.Id)!= null)
+                throw new ArgumentException("Duplicate Stations");
+            DataSource.ListLineStations.Add(lineStation);
+
+            //DataSource.ListLineStations.Add( 
+            //    new LineStation
+            //    {
+            //        Id = DS.DataSource.LineStationId++,
+            //        LineId = lineStation.LineId,
+            //        StationKey = lineStation.StationKey,
+            //        RankInLine = lineStation.RankInLine
+            //    });
+        }
+
+        public void DeleteLineStation(int id)
+        {
+            var lineStationToDelete = DataSource.ListLineStations.Where(t => t.Id == id).FirstOrDefault();
+            DataSource.ListLineStations.Remove(lineStationToDelete);
+        }
+
+        public IEnumerable<LineStation> GetAllLineStations()
+        {
+            return from stat in DataSource.ListLineStations
+                   select stat.Clone();
+        }
+
+        public IEnumerable<LineStation> GetAllLineStationsBy(Predicate<LineStation> predicate)
+        {
+            return from stat in DataSource.ListLineStations
+                   where predicate(stat)
+                   select stat.Clone();
+        }
+
+        public LineStation GetLineStation(int id)
+        {
+            return DataSource.ListLineStations.FirstOrDefault(s => s.Id == id).Clone();
+        }
+
+
+        public void UpdateLineStation(LineStation station)
+        {
+            LineStation tempStat = DataSource.ListLineStations.FirstOrDefault(s => s.Id == station.Id);
+            if (tempStat != null)
+            {
+                DataSource.ListLineStations.Remove(tempStat);
+                DataSource.ListLineStations.Add(station);
+            }
+            //else throw that line station doesn't exist
+        }
+
+        public void UpdateLineStation(int id, Action<LineStation> update)
         {
             throw new NotImplementedException();
         }
-
-        public void UpdateFollowingStations(BusStation station)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void UpdateFollowingStations(int id, Action<BusStation> update)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void UpdateLine(BusStation station)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void UpdateLine(int id, Action<BusStation> update)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void UpdateLineStation(BusStation station)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void UpdateLineStation(int id, Action<BusStation> update)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void UpdateStation(BusStation station)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void UpdateStation(int id, Action<BusStation> update)
-        {
-            throw new NotImplementedException();
-        }
-       
-
+        #endregion
 
     }
 }
