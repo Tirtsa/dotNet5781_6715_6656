c37f94c1cfd865ad591d6932c0c5253c913cc495
Tirtsah Barzilai <tirtsabarzilai@gmail.com>
2021-01-13 23:00:47 +0200

    End of BusStation features

--- WPF_UI/AddStationWindow.xaml.cs
+++ WPF_UI/AddStationWindow.xaml.cs
@@ -29,12 +29,35 @@ namespace WPF_UI
 
         private void AddStationButton_Click(object sender, RoutedEventArgs e)
         {
-            bl.AddStation(new BO.BusStation
+            try
             {
-                BusStationKey = int.Parse(busStationKeyTextBox.Text),
-                Address = addressTextBox.Text,
-                StationName = stationNameTextBox.Text
-            });
+                int stationCode;
+                int.TryParse(busStationKeyTextBox.Text, out stationCode);
+                bl.AddStation(new BO.BusStation
+                {
+                    BusStationKey = stationCode,
+                    Address = addressTextBox.Text,
+                    StationName = stationNameTextBox.Text
+                });
+                MessageBox.Show("התחנה נוספה בהצלחה");
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(" : ארעה תקלה/שגיאה" + ex);
+            }
+            
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
         }
     }
 }
